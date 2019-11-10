15a2753ed7ffcd30ec14311a59e00277d58d910a
David Rubinger <david.rubinger@gmail.com>
2019-11-10 13:31:40 -0500

    Add project as completed in Udacity workspace

--- etl.py
+++ etl.py
@@ -4,64 +4,138 @@ import psycopg2
 import pandas as pd
 from sql_queries import *
 
+pd.options.mode.chained_assignment = None  # default='warn'
 
 def process_song_file(cur, filepath):
+    """
+    Read song file and insert info into songs and artists tables.
+    
+    Reads in a file in data/song_data/, extracts song and artist info, and inserts
+    info into songs and artists dim tables.
+    
+    Arguments:
+        cur: the cursor object. 
+        filepath: log data file path. 
+
+    Returns:
+        None
+    """
+    
     # open song file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # insert song record
-    song_data = 
+    song_data = (
+        df[["song_id", "title", "artist_id", "year", "duration"]].
+        values.
+        tolist()[0]
+    )
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = 
+    artist_data = (
+        df[["artist_id", "artist_name", "artist_location", "artist_latitude", "artist_longitude"]].
+        values.
+        tolist()[0]
+    )
     cur.execute(artist_table_insert, artist_data)
 
-
 def process_log_file(cur, filepath):
-    # open log file
-    df = 
-
-    # filter by NextSong action
-    df = 
-
-    # convert timestamp column to datetime
-    t = 
+    """
+    Read log file and insert info into song play, user and time tables.
     
-    # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    Reads in a file in data/log_data/, extracts user and time info, and inserts
+    info into user and time dim tables. Also combines that data with data in
+    songs table to populate the songplays fact table.
+    
+    Arguments:
+        cur: the cursor object. 
+        filepath: log data file path. 
 
+    Returns:
+        None
+    """
+    
+    # open log file
+    df = pd.read_json(filepath, lines=True)
+    
+    # filter by NextSong action
+    df = df.query("page == 'NextSong'")
+    
+    # convert stamp column to datetime and extract time-related features
+    df.loc[:, "ts"] = pd.to_datetime(df["ts"], unit='ms')
+    df.loc[:, "hour"] = df["ts"].apply(lambda x: x.hour)
+    df.loc[:, "day"] = df["ts"].apply(lambda x: x.day)
+    df.loc[:, "week"] = df["ts"].apply(lambda x: x.week)
+    df.loc[:, "month"] = df["ts"].apply(lambda x: x.month)
+    df.loc[:, "year"] = df["ts"].apply(lambda x: x.year)
+    df.loc[:, "weekday"] = df["ts"].apply(lambda x: x.dayofweek)
+    
+    time_df = (
+        df.loc[:, ["ts", "hour", "day", "week", "month", "year", "weekday"]].drop_duplicates()
+    )
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = (
+        df.
+        loc[:,["userId", "firstName", "lastName", "gender", "level"]].
+        rename({"userId": "user_id",
+                "firstName": "first_name",
+                "lastName": "last_name"},
+               axis="columns").
+        drop_duplicates()
+    )
 
     # insert user records
     for i, row in user_df.iterrows():
         cur.execute(user_table_insert, row)
-
+    
     # insert songplay records
     for index, row in df.iterrows():
         
         # get songid and artistid from song and artist tables
-        results = cur.execute(song_select, (row.song, row.artist, row.length))
-        songid, artistid = results if results else None, None
+        cur.execute(song_select, (row.song, row.artist, row.length))
+        results = cur.fetchone()
+        
+        if results:
+            songid, artistid = results
+        else:
+            songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data = (
+            row.ts, row.userId, row.level,
+            songid, artistid, row.sessionId, row.location,
+            row.userAgent
+        )
         cur.execute(songplay_table_insert, songplay_data)
 
-
 def process_data(cur, conn, filepath, func):
+    """
+    Apply song and log processing functions to all data files.
+    
+    Applies process_song_file() and process_log_file() to every file in
+    data/song_data/ and data/log_data/, respectively.
+    
+    Arguments:
+        cur: the cursor object.
+        conn: database connection object.
+        filepath: data/song_data/ or data/log_data/ file path.
+        func: process_song_file or process_log_file function.
+
+    Returns:
+        None
+    """
+    
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
         files = glob.glob(os.path.join(root,'*.json'))
         for f in files :
             all_files.append(os.path.abspath(f))
+    all_files = [file for file in all_files if ".ipynb_checkpoints" not in file]
 
     # get total number of files found
     num_files = len(all_files)
@@ -72,7 +146,6 @@ def process_data(cur, conn, filepath, func):
         func(cur, datafile)
         conn.commit()
         print('{}/{} files processed.'.format(i, num_files))
-
 
 def main():
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
--- sql_queries.py
+++ sql_queries.py
@@ -1,49 +1,110 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songplays (
+        start_time timestamp NOT NULL,
+        user_id int NOT NULL,
+        level varchar,
+        song_id char (18) NOT NULL,
+        artist_id char (18) NOT NULL,
+        session_id int,
+        location varchar,
+        user_agent varchar,
+        songplay_id SERIAL PRIMARY KEY
+    )
 """)
 
 user_table_create = ("""
+    CREATE TABLE IF NOT EXISTS users (
+        user_id int PRIMARY KEY,
+        first_name varchar,
+        last_name varchar,
+        gender char (1),
+        level varchar
+    )
 """)
 
 song_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songs (
+        song_id char (18) PRIMARY KEY,
+        title varchar,
+        artist_id char (18),
+        year int,
+        duration numeric
+    )
 """)
 
 artist_table_create = ("""
+    CREATE TABLE IF NOT EXISTS artists (
+        artist_id char (18) PRIMARY KEY,
+        artist_name varchar,
+        artist_location varchar,
+        artist_latitude decimal,
+        artist_longitude decimal
+    )
 """)
 
 time_table_create = ("""
+    CREATE TABLE IF NOT EXISTS time (
+        start_time timestamp PRIMARY KEY,
+        hour int,
+        day int,
+        week int,
+        month int,
+        year int,
+        weekday int
+    )
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+    INSERT INTO songplays VALUES (%s, %s, %s, %s, %s, %s, %s, %s, DEFAULT)
 """)
 
 user_table_insert = ("""
+    INSERT INTO users VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (user_id) DO UPDATE
+        SET level = EXCLUDED.level;
 """)
 
 song_table_insert = ("""
+    INSERT INTO songs VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (song_id) DO NOTHING
 """)
 
 artist_table_insert = ("""
+    INSERT INTO artists VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (artist_id) DO NOTHING
 """)
 
-
 time_table_insert = ("""
+    INSERT INTO time VALUES (%s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT (start_time) DO NOTHING
 """)
 
 # FIND SONGS
 
 song_select = ("""
+    SELECT
+        s.song_id,
+        s.artist_id
+    FROM
+        songs s
+    LEFT JOIN
+        artists a on s.artist_id = a.artist_id
+    WHERE
+        s.title = %s
+        AND a.artist_name = %s
+        AND s.duration = %s;
 """)
 
 # QUERY LISTS
